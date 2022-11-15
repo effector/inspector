7164ac526aa60a24bb8676caebdf1a96b9491448
Sergey Sova <mail@sergeysova.com>
2022-11-15 14:02:57 +0400

    chore: migrate from parcel to vitejs

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import babel from '@rollup/plugin-babel';
+import legacy from '@vitejs/plugin-legacy';
+import solid from 'vite-plugin-solid';
+
+export default defineConfig({
+  root: './usage',
+  plugins: [
+    babel({ extensions: ['.ts', '.tsx'], babelrc: true }) as any,
+    legacy({
+      targets: ['last 4 versions', 'not IE 11'],
+    }),
+    solid(),
+  ],
+  resolve: {
+    alias: [{ find: '~', replacement: './src' }],
+  },
+});
